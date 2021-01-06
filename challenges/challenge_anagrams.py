14e6eac61146bbd5dab734e0d4bc2344b9edb48b
Gustaff <gustavohmfigueiredo@gmail.com>
2021-01-06 17:00:38 -0300

    retry time

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,8 +1,40 @@
-from challenges.challenge_find_the_duplicate import merge_sort
+def merge_sort(word):
+    word_size = len(word)
+    if word_size <= 1:
+        return word
 
+    half = word_size // 2
+
+    list1, list2 = merge_sort(word[:half]), merge_sort(word[half:])
+
+    return merge(list1, list2)
+
+
+def merge(l1, l2):
+    size1, size2 = len(l1), len(l2)
+    i, j = 0, 0
+    S = []
+
+    while i < size1 and j < size2:
+        if l1[i] < l2[j]:
+            S.append(l1[i])
+            i += 1
+        else:
+            S.append(l2[j])
+            j += 1
+
+    while i < size1:
+        S.append(l1[i])
+        i += 1
+
+    while j < size2:
+        S.append(l2[j])
+        j += 1
+
+    return S
 
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string:
+    if first_string == '' or second_string == '':
         return False
     first_ordenated = merge_sort(first_string)
     second_ordenated = merge_sort(second_string)
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,7 +2,7 @@ def is_palindrome_iterative(word):
     size = len(word)
     if size == 0:
         return False
-    for i in range(size):
+    for i in range(size // 2):
         if word[i] != word[size - 1 - i]:
             return False
     return True
