c53b55fee78955dfe98a529c60a4d174ec525323
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-06 18:42:42 -0300

    test req3 with array

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    lista1 = []
+    lista2 = []
+
+    lista1.append(first_string)
+    lista2.append(second_string)
+
+    if lista1.len == lista2.len:
+        return True
+    else:
+        return False
