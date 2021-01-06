c67896099d1ea1f89ccb85e59368a7a9780ee162
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-06 17:40:50 -0300

    Desafio 2 resolvido

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low, high):
     if not word:
         return False
+    if low >= high:
+        return True
+    if high > low:
+        if word[low] == word[high]:
+            is_palindrome_recursive(word, low + 1, high - 1)
+        else:
+            return False
