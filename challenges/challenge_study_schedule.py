6228817f3c39ff238495ae50421145cf7f463d35
nato-re <alvares.renato@hotmail.com>
2021-01-06 17:12:01 -0300

    começando projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if not target_time or target_time > 12:
+        return 0
+    count = 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            count += 1
+    return count
+
+
+    
+
+
+start_time = [2, 1, 2, 1, 4, 4]
+end_time   = [2, 2, 3, 5, 5, 5]
+
+study_schedule(start_time, end_time, 1)
