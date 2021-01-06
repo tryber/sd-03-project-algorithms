c677049d748efd0d61033da4182639c88e8bd6e9
luldelrey <rpgstrikered@gmail.com>
2021-01-06 16:51:41 -0300

    algorithms refactored, see if their complexity is ok

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,18 +1,42 @@
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+    return merged
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+    return merge(left, right, arr.copy())
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     if not first_string or not second_string:
         return False
-    a = "".join(sorted(first_string))
-    b = "".join(sorted(second_string))
+    a = "".join(merge_sort(list(first_string)))
+    b = "".join(merge_sort(list(second_string)))
     for i in range(len(a)):
         if a[i] != b[i]:
             return False
     return True
 
 
-if __name__ == '__main__':
-    a = 'pedra'
-    b = 'perda'
+if __name__ == "__main__":
+    a = "pedra"
+    b = "perda"
     print(is_anagram(a, b))
     a = 'pedra'
     b = 'pedraaa'
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,31 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if len(nums) > 1:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if type(nums[i]) == str or nums[i] < 0:
+                return False
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+    return False
+
+
+if __name__ == '__main__':
+    nums = [1, 3, 4, 2, 2]
+    print(find_duplicate(nums) == 2)
+    nums = [3, 1, 3, 4, 2]
+    print(find_duplicate(nums) == 3)
+    nums = [1, 1, 2]
+    print(find_duplicate(nums) == 1)
+    nums = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
+    print(find_duplicate(nums) == 7)
+    nums = [1]
+    print(find_duplicate(nums))
+    nums = [-1, -1]
+    print(find_duplicate(nums))
+    nums = ['a', 'b']
+    print(find_duplicate(nums))
+    nums = [1, 1]
+    print(find_duplicate(nums) == 1)
+    nums = [1, 2]
+    print(find_duplicate(nums) == 1)
