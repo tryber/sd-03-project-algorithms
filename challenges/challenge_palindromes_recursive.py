68946d1e8ba21c24bc6eb6e25536dff9a90a63de
Cardoso <lsc.cardosoo@gmail.com>
2021-01-06 12:28:02 -0300

    Primeiro commit #VQV

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+   if word == '':
+       return False
+   if (len(word) // 2 == low):
+       return True
+   if (word[low] == word[high]):
+       return is_palindrome_recursive(word, low + 1, high - 1)
+   else:
+       return False
