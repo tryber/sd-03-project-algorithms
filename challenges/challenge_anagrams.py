083f7270cc2dc4869ad7e994af441fe17dc3d746
luis.campos <gucampos845@gmail.com>
2021-01-06 18:54:03 -0300

    Requisito 3 - Strings vazias

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,4 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    anagram = []
     if first_string == '' or second_string == '':
         return False
-
-    for i in range(len(first_string)):
-        for j in range(len(first_string)):
-            if second_string[j] == first_string[i]:
-                anagram[j] = first_string[i]
-                return True
-            else:
-                return False
