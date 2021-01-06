8eab90e9dec37c30bf132e13dd32b2e8edb5c8fa
potew <potew0@gmail.com>
2021-01-06 17:49:19 -0300

    Challenge no. 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+# Estudante   1  2  3  4  5  6
+# starTime = [2, 1, 2, 1, 4, 4]
+# endTime = [2, 2, 3, 5, 5, 5]
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    target_time_counter = 0
+
+    for i in range(len(end_time)):
+        if target_time in range(start_time[i], end_time[i] + 1):
+            target_time_counter += 1
+
+    return target_time_counter
+
+
+# horario = input('Digite o target time: ')
+# print(study_schedule(starTime, endTime, int(horario)))
