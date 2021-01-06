923076b9923898e08381a4ba02a4867a5d4f7803
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-06 18:33:27 -0300

    test req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,12 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    if first_string == ' ' and second_string == ' ':
+    nova_primeira_palavra = first_string.sort()
+    nova_segunda_palavra = second_string.sort()
+
+    if first_string == '' or second_string == '':
         return False
+
+    if nova_primeira_palavra == nova_segunda_palavra:
+        return True
+    else:
+        False
