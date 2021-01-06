368d288dc003b26ea4d2ca61b81f0712477f11f9
Cardoso <lsc.cardosoo@gmail.com>
2021-01-06 12:30:17 -0300

    Flake8

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-   if word == '':
-       return False
-   if (len(word) // 2 == low):
-       return True
-   if (word[low] == word[high]):
-       return is_palindrome_recursive(word, low + 1, high - 1)
-   else:
-       return False
+    if word == '':
+        return False
+    if (len(word) // 2 == low):
+        return True
+    if (word[low] == word[high]):
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    else:
+        return False
