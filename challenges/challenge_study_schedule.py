583d78d0269f9c9a2d21e28a305dc14b9e587dab
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-06 16:58:54 -0300

    Primeiro commit, test-req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    melhor_horario = 0
+    if start_time == '' or target_time == '':
+        return 0
+
+    for i in range(len(start_time)):
+        if start_time[i] and target_time <= end_time[i]:
+            melhor_horario += 1
+
+    return melhor_horario
