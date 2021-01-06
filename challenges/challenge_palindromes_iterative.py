eb6eaa6647c14ba4572800271b3a2aa348f1307e
luldelrey <rpgstrikered@gmail.com>
2021-01-06 17:08:57 -0300

    all done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,23 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    len_word = len(word)
+    reverse_str = word[::-1]
+    for i in range(len_word):
+        if word[i] != reverse_str[i]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    word = ""
+    print(is_palindrome_iterative(word))
+    word = "AGUA"
+    print(is_palindrome_iterative(word))
+    word = "ANA"
+    print(is_palindrome_iterative(word))
+    word = "SOCOS"
+    print(is_palindrome_iterative(word))
+    word = "REVIVER"
+    print(is_palindrome_iterative(word))
