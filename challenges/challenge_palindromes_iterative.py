c836719ea3a7900c98bdb982de8842b039fbe232
dax <a130120642@gmail>
2021-01-06 15:50:46 -0300

    add iterative palindromes

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,5 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    for x in range(len(word)//2):
+        if word[x] != word[-(x+1)]:
+            return False
+    return bool(word)
