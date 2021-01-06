de96086e0ae3957cfecbc890ae9f95bad5cd67e8
luldelrey <rpgstrikered@gmail.com>
2021-01-06 15:33:34 -0300

    palindrome recursive done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,22 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    limite = len(word) // 2
+    if not word or word[low] != word[high]:
+        return False
+    elif low > limite:
+        return True
+    else:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+
+if __name__ == '__main__':
+    word = ""
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    word = "AGUA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    word = "ANA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    word = "SOCOS"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    word = "REVIVER"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
