47cad80298af08d1233e7eeb843d362a2006c102
luldelrey <rpgstrikered@gmail.com>
2021-01-06 15:20:14 -0300

    study challenge done and passing

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    best_time = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            best_time += 1
+    return best_time
+
+
+if __name__ == '__main__':
+    start_time = [2, 1, 2, 1, 4, 4]
+    end_time = [2, 2, 3, 5, 5, 5]
+
+    print(study_schedule(start_time, end_time, 5))
+    print(study_schedule(start_time, end_time, 4))
+    print(study_schedule(start_time, end_time, 3))
+    print(study_schedule(start_time, end_time, 2))
+    print(study_schedule(start_time, end_time, 1))
