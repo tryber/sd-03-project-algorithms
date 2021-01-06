79fd1b1399c1b1bca1194ab6d7f568fe15bc9fbd
nato-re <alvares.renato@hotmail.com>
2021-01-06 18:06:56 -0300

    test lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,7 +5,7 @@ def is_palindrome_recursive(word, low, high):
     last = len(word) - 1
     if len(word) == 1:
         return True
-    if word[low] == word[last]:
+    if word[low] == word[last]:          
         return is_palindrome_recursive(
             word[1 : len(word) - 1], 0, len(word) - 1
         )
