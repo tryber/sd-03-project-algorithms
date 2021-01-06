e60e186c6733479fbf1db1fe301d783f234f591d
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-06 18:12:11 -0300

    test req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,4 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    if first_string == '' and second_string == '':
+    if first_string == ' ' and second_string == ' ':
         return False
