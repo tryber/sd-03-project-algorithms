fe54dbedb55dd0aea1133b3774f2d98fdf7b01d9
dax <a130120642@gmail>
2021-01-06 15:43:34 -0300

    add recursive palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    elif word[0] == word[-1]:
+        if len(word) <= 3:
+            return True
+        return is_palindrome_recursive(word[1:-1], None, None)
+    return False
