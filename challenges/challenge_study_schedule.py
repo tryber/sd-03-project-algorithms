aa0a273897973f6662b326e6af65c0db9598f395
luis.campos <gucampos845@gmail.com>
2021-01-06 16:59:04 -0300

    Iniciando projeto de algoritimos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    count = 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            count += 1
+
+    return count
