9bc39e58579236c427f76fa64903a39113e828d5
dax <a130120642@gmail>
2021-01-06 18:18:50 -0300

    last, I hope

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,27 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if nums:
+        nums = merge_sort(nums)
+        for x in range(0, len(nums)-1):
+            if nums[x] == nums[x+1]:
+                return nums[x]
+    return False
+
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    else:
+        half = len(arr)//2
+        return merge(merge_sort(arr[:half]), merge_sort(arr[half:]))
+
+# merges two sorted arrays
+def merge(a, b):
+    merged = list()
+    while len(a) and len(b):
+        highest = a
+        if b[0] < a[0]:
+            highest = b
+        merged.append(highest.pop(0))
+    last = (a or b)
+    while last:
+        merged.append(last.pop(0))
+    return merged
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if target_time == None or max((len(start_time),len(end_time))) < 1:
+        return 0
+    people_in_time = dict()
+    for x in range(len(start_time)):
+        for x in range(start_time[x], end_time[x]+1):
+            if x not in people_in_time.keys():
+                people_in_time |= {x:1}
+            else:
+                people_in_time[x] += 1
+    # maximum_people = max(people_in_time.items(), key=lambda item:item[1])
+    return people_in_time[target_time]
