5fc50ff536785db773ac03b93dacc4286d63c79d
nato-re <alvares.renato@hotmail.com>
2021-01-06 18:11:18 -0300

    lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,12 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
-    if not word or high < 1:
-        return False
     last = len(word) - 1
     if len(word) == 1:
         return True
-    if word[low] == word[last]:
+    elif word[low] == word[last]:
         return is_palindrome_recursive(
-            word[1 : len(word) - 1], 0, len(word) - 1
+            word[1: len(word) - 1], 0, len(word) - 1
         )
-    elif word[low] != word[last]:
+    elif word[low] != word[last] or not word or high < 1:
         return False
     else:
         return False
