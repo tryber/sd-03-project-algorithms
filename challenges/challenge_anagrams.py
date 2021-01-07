89f44fe4b9e34a1b8a53ff4adfa10e4e186dd20e
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:28:06 -0300

    False in string empty

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,8 +7,12 @@ def is_anagram(first_string, second_string):
     for i in second_string:
         arr2.append(i)
         arr2.sort()
+    if not first_string or not second_string:
         return False
     if arr1 == arr2:
         return True
     else:
-        False
+        return False
+
+
+print(is_anagram('dd', 'd'))
