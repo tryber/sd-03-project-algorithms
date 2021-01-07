2ec2ffb437ba324a0164056217092cd22b32d4f7
William Igor <williamigorsilva@gmail.com>
2021-01-07 19:07:46 -0300

    One more

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
     if not start_time or not target_time:
         return 0
+
+    count = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time and end_time >= target_time[i]:
+            count = 0   
+    return count
