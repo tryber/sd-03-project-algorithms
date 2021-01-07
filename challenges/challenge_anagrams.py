52dca76d45f512cc1fe6a40ff098e8ec49836954
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-07 17:58:12 -0300

    test req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,12 +1,6 @@
-def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+def ordenar_anagrama(first_string, second_string):
     lista1 = []
     list2 = []
-
-    if first_string == "" or second_string == "":
-        return False
-    elif len(first_string) != len(second_string):
-        return False
 
     for i in range(len(first_string)):
         lista1.append(first_string[i]).sort()
@@ -15,10 +9,17 @@ def is_anagram(first_string, second_string):
     for j in range(len(second_string)):
         list2.append(second_string[j]).sort()
         return list2
-
     if lista1 == list2:
         return True
     else:
+        return False
+
+
+def is_anagram(first_string, second_string):
+    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    elif len(first_string) != len(second_string):
         return False
 
 
