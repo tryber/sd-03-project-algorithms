be247f2b57ab89155712e8b1543f6d6a220e45d4
Gustaff <gustavohmfigueiredo@gmail.com>
2021-01-07 14:38:59 -0300

    retry

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -37,6 +37,7 @@ def merge(l1, l2):
 def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
         return False
+
     first_ordenated = merge_sort(first_string)
     second_ordenated = merge_sort(second_string)
 
