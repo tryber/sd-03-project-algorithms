fb185a0c49675615fe28ba1a524564c608607ef9
potew <potew0@gmail.com>
2021-01-07 19:08:34 -0300

    Casespeciais

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,7 @@
 def is_palindrome_recursive(word, low, high):
-    if len(word) > 0 and low >= high:
+    if len(word) == 0:
+        return False
+    if low >= high:
         return True
     if word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
@@ -9,3 +11,4 @@ def is_palindrome_recursive(word, low, high):
 # print(is_palindrome_recursive('AABCBAAB', 0, 7))
 # print(is_palindrome_recursive('ABACATE', 0, 6))
 # print(is_palindrome_recursive('ACAIACA', 0, 6))
+# print(is_palindrome_recursive('', 0, -1))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,13 +5,12 @@
 
 def study_schedule(start_time, end_time, target_time):
     target_time_counter = 0
-
-    for i in range(len(end_time)):
-        if target_time in range(start_time[i], end_time[i] + 1):
-            target_time_counter += 1
+    if len(start_time) > 0:
+        for i in range(len(end_time)):
+            if target_time in range(start_time[i], end_time[i] + 1):
+                target_time_counter += 1
 
     return target_time_counter
-
 
 # horario = input('Digite o target time: ')
 # print(study_schedule(starTime, endTime, int(horario)))
