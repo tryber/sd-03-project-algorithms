6e79e6b93e96e887a699b17792f44725e2cf2b57
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:13:39 -0300

    Resolve issues

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,6 +3,3 @@ def is_palindrome_recursive(word, low, high):
         return True
     else:
         return low == high and is_palindrome_recursive(word[1: -1], low, high)
-    
-
-# print(is_palindrome_recursive('natan','n', 'n'));
