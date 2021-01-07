90a8d2fd90c1dd6495c2cd9ef47587d39365d494
luizfernando <luizfernandotic@gmail.com>
2021-01-07 18:35:57 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            counter += 1
+
+    return counter
+
+
+""" # para testes locais
+start_time = [1, 2, 3, 4, 5]
+end_time = [5, 4, 3, 4, 5]
+target_time = 0
+print(study_schedule(start_time, end_time, target_time)) """
