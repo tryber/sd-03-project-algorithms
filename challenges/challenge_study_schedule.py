3b3e74c5ad665ae988a11a12672226fbc938cb7d
William Igor <williamigorsilva@gmail.com>
2021-01-07 19:09:58 -0300

    Spaces is trouble

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,5 +5,5 @@ def study_schedule(start_time, end_time, target_time):
     count = 0
     for i in range(len(start_time)):
         if start_time[i] <= target_time and end_time >= target_time[i]:
-            count = 0   
+            count = 0
     return count
