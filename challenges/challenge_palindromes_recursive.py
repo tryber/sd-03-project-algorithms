d1e82a9116434a6256afb8ff7b554cfcfe422408
DevT <aguiarthiag@gmail.com>
2021-01-07 17:28:31 -0300

    Required passing locally

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) < 2:
+        return True
+    return (word[0] == word[-1] and is_palindrome_recursive(
+        word[1:len(word)-1], low, high))
