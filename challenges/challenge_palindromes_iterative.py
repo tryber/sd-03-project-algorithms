5a82944b69ff13df7fb32e577f1aedcc16e757bf
Hfreitas <hfreitas86@gmail.com>
2021-01-07 17:01:17 -0300

    requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,28 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    search_limit = len(word) // 2
+
+    if len(word) == 1:
+        return True
+    elif len(word) == 0:
+        return False
+    else:
+        for i in range(search_limit):
+            if word[i] == word[len(word) - i - 1]:
+                return True
+            return False
+
+
+if __name__ == "__main__":
+    word = ""
+    print(is_palindrome_iterative(word))
+    word = "ANA"
+    print(is_palindrome_iterative(word))
+    word = "AGUA"
+    print(is_palindrome_iterative(word))
+    word = "SOCOS"
+    print(is_palindrome_iterative(word))
+    word = "REVIVER"
+    print(is_palindrome_iterative(word))
+    word = "COXINHA"
+    print(is_palindrome_iterative(word))
