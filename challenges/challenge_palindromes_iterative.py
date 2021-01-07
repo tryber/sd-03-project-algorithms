1b59e6dacda728c7188aa6d418f71997636fec9d
luldelrey <rpgstrikered@gmail.com>
2021-01-07 15:18:17 -0300

    refactoring

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,9 +2,9 @@ def is_palindrome_iterative(word):
     """ Faça o código aqui. """
     if not word:
         return False
-    len_word = len(word)
+    len_word = len(word) - 1
     for i in range(len_word):
-        if word[i] != word[len_word - 1 - i]:
+        if word[i] != word[len_word - i]:
             return False
     return True
 
