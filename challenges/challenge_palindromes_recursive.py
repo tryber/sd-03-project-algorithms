302a818f5c57fe1a4a452487862be23f394d84d5
potew <potew0@gmail.com>
2021-01-07 17:36:43 -0300

    Desbugada a 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,11 @@
 def is_palindrome_recursive(word, low, high):
-    if low >= high:
+    if len(word) > 0 and low >= high:
         return True
-    else:
-        if len(word) > 0 and word[low] == word[high]:
-            low += 1
-            high -= 1
-            is_palindrome_recursive(word, low, high)
-        else:
-            return False
+    if word[low] == word[high]:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    return False
 
 
-print(is_palindrome_recursive('AABBAA', 0, 5))
+# print(is_palindrome_recursive('AABCBAAB', 0, 7))
+# print(is_palindrome_recursive('ABACATE', 0, 6))
+# print(is_palindrome_recursive('ACAIACA', 0, 6))
