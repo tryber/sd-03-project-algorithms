7f2f7042eff3a0fbae59105658e21f9c3115a008
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-07 17:44:17 -0300

    Favor analizar o comentario do requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,7 +2,14 @@ def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     if first_string == "" or second_string == "":
         return False
+    elif len(first_string) != len(second_string):
+        return False
+
+    """  codigo com bug passando em
+todos os casos sem cumprir
+todos os requisitos
+
     while len(first_string) != len(second_string):
         return False
     else:
-        return True
+        return True """
