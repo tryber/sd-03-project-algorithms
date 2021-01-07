67158e78f9a7229e15856370391cbdf5fc6b03d4
William Igor <williamigorsilva@gmail.com>
2021-01-07 18:57:36 -0300

    I think to myself

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,5 @@
 def is_palindrome_recursive(word, low, high):
-    if len(word) > 0 and word == word[:: -1]:
+    if len(word) > 0 and low >= high:
         return True
     if word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
