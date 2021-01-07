ae0712b64621d367c2256385f2b2e13ff3bab502
William Igor <williamigorsilva@gmail.com>
2021-01-07 18:51:16 -0300

    Lets see this problem

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,6 @@
 def is_palindrome_recursive(word, low, high):
-    if not word:
-        return False
-    # if word[:: -1] != word:
-    #     return False
-    # else:
-    #     return low == high and is_palindrome_recursive(word, low, high)
-    # if len(word) == 2:  # base case to stop
-    #     return False
+    if len(word) > 0 and word == word[:: -1]:
+        return True
     if word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
     return False
