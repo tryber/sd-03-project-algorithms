c7688ff4f0342cf4949a7b7bdc3192c2534231d2
dax <a130120642@gmail>
2021-01-07 16:13:11 -0300

    schedules

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,6 @@
 def study_schedule(start_time, end_time, target_time):
-    if target_time is None or max((len(start_time), len(end_time))) < 1:
-        return 0
-    people_in_time = dict()
+    counter = 0
     for x in range(len(start_time)):
-        for x in range(start_time[x], end_time[x] + 1):
-            if x not in people_in_time.keys():
-                people_in_time |= {x: 1}
-            else:
-                people_in_time[x] += 1
-    # maximum_people = max(people_in_time.items(), key=lambda item:item[1])
-    return people_in_time[target_time]
+        if target_time in range(start_time[x], end_time[x]+1):
+            counter += 1
+    return counter
