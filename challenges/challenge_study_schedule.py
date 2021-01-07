0d59ff0225e0983661b4d23fba8f3debd6d647c4
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-07 16:12:58 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    for i in range(len(start_time)):
+        if end_time[i] >= target_time and start_time[i] <= target_time:
+            count += 1
+
+    return count
+
+
+print(study_schedule(start_time, end_time, 3))
