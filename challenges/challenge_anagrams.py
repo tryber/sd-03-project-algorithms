9602c93f1cca47c7ac062160883bcae17e510f0e
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:24:49 -0300

    anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    ""
+    arr1 = []
+    arr2 = []
+    for i in first_string:
+        arr1.append(i)
+        arr1.sort()
+    for i in second_string:
+        arr2.append(i)
+        arr2.sort()
+        return False
+    if arr1 == arr2:
+        return True
+    else:
+        False
