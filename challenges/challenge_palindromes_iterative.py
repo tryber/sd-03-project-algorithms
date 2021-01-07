9bcabe217c119281e63554b68ddc9943e4fe0a6b
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-07 15:20:53 -0300

    Requisito 5 - Bonus - Resolvido

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,15 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    low = 0
+    high = len(word) - 1
+    if not word:
+        return False
+    for i in range(len(word)):
+        if low >= high:
+            return True
+        if word[high] == word[low]:
+            low = low + 1
+            high = high - 1
+    return False
+
+
+print(is_palindrome_iterative("marcoocram"))
