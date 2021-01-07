302c619eb020f2e22b2fb5be9d703b3716073421
William Igor <williamigorsilva@gmail.com>
2021-01-07 19:19:04 -0300

    I perceived in nigth i dont think very well

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
-    if not start_time or not target_time:
+    if not start_time or not end_time:
         return 0
 
     count = 0
     for i in range(len(start_time)):
         if start_time[i] <= target_time <= end_time[i]:
-            count = 0
+            count += 1
     return count
