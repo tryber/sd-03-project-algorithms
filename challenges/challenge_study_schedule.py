72d0683dd476b51eaa0c17805605d3293a97cb68
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:48:06 -0300

    Ok I verify two conditions in IF request 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,3 @@
 def study_schedule(start_time, end_time, target_time):
-    if not start_time:
-        return 0
-    if not target_time:
+    if not start_time or not target_time:
         return 0
