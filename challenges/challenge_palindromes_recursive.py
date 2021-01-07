8926ffb0111bde31980145ef1b066e57c608df20
William Igor <williamigorsilva@gmail.com>
2021-01-07 15:16:58 -0300

    Commit pull

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,6 @@
 def is_palindrome_recursive(word, low, high):
     if word == '':
         return False
-    if low[0] == high[-1]:
+    if low == high:
         return True
-    return is_palindrome_recursive(word[::-1])
+    return is_palindrome_recursive(word[::-1], low, high)
