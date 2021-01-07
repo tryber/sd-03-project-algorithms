b68486564943d97b95d10c3e10ec7005083c43cc
luis.campos <gucampos845@gmail.com>
2021-01-07 17:09:37 -0300

    Teste requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,3 +2,5 @@ def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     if first_string == '' or second_string == '':
         return False
+    if len(first_string) != len(second_string):
+        return False
