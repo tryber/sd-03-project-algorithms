d5745a3f5f0345fd0d0c23c41d854bda24efcbce
fabemiliano <fab.emiliano@gmail.com>
2021-01-07 19:30:54 -0300

    requiste 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_sorted = list(first_string)
+    second_sorted = list(second_string)
+    first_sorted.sort()
+    second_sorted.sort()
+    if first_sorted == second_sorted:
+        return True
+    else:
+        return False
