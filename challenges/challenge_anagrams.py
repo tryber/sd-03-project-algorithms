385c45956afb0645349d103fb000b960a3ceac62
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-07 17:28:43 -0300

    requisitos 1 2 3 funcionando

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+from challenges.sorts.merge_sort import merge_sort
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    string_one_sorted = merge_sort(first_string)
+    string_two_sorted = merge_sort(second_string)
+
+    if string_one_sorted == string_two_sorted:
+        return True
+    else:
+        return False
+
+
+string_one = 'pedra'
+string_two = 'perda'
+
+print(is_anagram(string_one, string_two))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,24 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    is_palindrome = None
+
+    if word == '':
+        return False
+
+    while low < high:
+        if word[low] == word[high]:
+            low += 1
+            high -= 1
+            is_palindrome = True
+            is_palindrome_recursive(word, low, high)
+        else:
+            is_palindrome = False
+            return is_palindrome
+
+    return is_palindrome
+
+
+word = 'AGUA'
+low = 0
+high = len(word)
+
+print(is_palindrome_recursive(word, low, high - 1))
