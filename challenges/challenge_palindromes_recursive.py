de87c39583ea12ebaef556a5c0f8c841f977bc0d
luis.campos <gucampos845@gmail.com>
2021-01-07 17:01:21 -0300

    Requisito 2 //

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,5 +6,7 @@ def is_palindrome_recursive(word, low, high):
         return False
     elif len(word) == 1:
         return True
+    elif high < len(word) // 2:
+        return True
     else:
         return is_palindrome_recursive(word, low + 1, high - 1)
