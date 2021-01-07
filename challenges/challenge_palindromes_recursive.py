c94c77e0631c23e2cc0b9d0b8f30a4c155cc7cdc
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:57:44 -0300

    This challenge is a challenge

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,7 @@
 def is_palindrome_recursive(word, low, high):
-    if len(word) <= 1:
-        return True
+    if not word:
+        return False
+    if word[:: -1] != word:
+        return False
     else:
-        return low == high and is_palindrome_recursive(word[1: -1], low, high)
+        return low == high and is_palindrome_recursive(word, low, high)
