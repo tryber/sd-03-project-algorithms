d56203edd457801a24f485f931f39375be2bb97f
William Igor <williamigorsilva@gmail.com>
2021-01-07 17:41:52 -0300

    Refactoring I guess is a good job i make

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,6 @@
 def is_anagram(first_string, second_string):
-    # arr1 = []
-    # arr2 = []
-    # for i in first_string:
-    #     arr1.append(i)
-    #     arr1.sort()
-    # for i in second_string:
-    #     arr2.append(i)
-    #     arr2.sort()
-    # i see this code and i think that isn't good.
-    #  so i search in google and i discovered a func sorted()
     string_one = sorted(first_string)
     string_two = sorted(second_string)
-    # print(string_one, "junto ", string_two)
     if not first_string or not second_string:
         return False
     if string_one == string_two:
