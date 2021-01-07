ba223d26ed6fa26a8007ea569c7fe65a59ae1cdb
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-07 17:31:31 -0300

    lint

--- challenges/sorts/merge_sort.py
+++ challenges/sorts/merge_sort.py
@@ -3,7 +3,7 @@ def merge_sort(string):
         return string
 
     mid = len(string) // 2
-   
+
     left, right = merge_sort(string[:mid]), merge_sort(string[mid:])
 
     new_string = string[:]
@@ -18,10 +18,18 @@ def merge(left, right, merged):
 
         # compare os dois itens das partes e insira no array de mistura o menor
         if left[left_cursor] <= right[right_cursor]:
-            merged = merged[:left_cursor + right_cursor] + left[left_cursor] + merged[left_cursor + right_cursor + 1:]
+            merged = (
+                merged[: left_cursor + right_cursor]
+                + left[left_cursor]
+                + merged[left_cursor + right_cursor + 1 :]
+            )
             left_cursor += 1
         else:
-            merged = merged[:left_cursor + right_cursor] + right[right_cursor] + merged[left_cursor + right_cursor + 1:] 
+            merged = (
+                merged[: left_cursor + right_cursor]
+                + right[right_cursor]
+                + merged[left_cursor + right_cursor + 1 :]
+            )
             right_cursor += 1
     # a iteração acima irá inserir os elementos de forma ordenada
 
@@ -31,11 +39,19 @@ def merge(left, right, merged):
     # itera sobre os elementos restantes na partição "esquerda"
     # inserindo-os no array de mistura
     for left_cursor in range(left_cursor, len(left)):
-        merged = merged[:left_cursor + right_cursor] + left[left_cursor] + merged[left_cursor + right_cursor + 1:]
+        merged = (
+            merged[: left_cursor + right_cursor]
+            + left[left_cursor]
+            + merged[left_cursor + right_cursor + 1 :]
+        )
 
     # itera sobre os elementos restantes na partição "direita"
     # inserindo-os no array de mistura
     for right_cursor in range(right_cursor, len(right)):
-        merged = merged[:left_cursor + right_cursor] + right[right_cursor] + merged[left_cursor + right_cursor + 1:]
+        merged = (
+            merged[: left_cursor + right_cursor]
+            + right[right_cursor]
+            + merged[left_cursor + right_cursor + 1 :]
+        )
 
     return merged
