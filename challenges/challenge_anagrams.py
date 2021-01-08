7eeebea5dc0cd95b9003056451781103aa63b5f8
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-08 16:35:14 -0300

    Test

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,10 +2,8 @@ def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     if first_string == "" or second_string == "":
         return False
-    elif len(first_string) != len(second_string):
+    if first_string != second_string:
         return False
-    if first_string == second_string:
-        return True
 
 
 """  codigo com bug passando em
