cb68d2f4b2b307909b8b38d010b79ae7328c02ad
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-08 10:07:10 -0300

    requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,31 +2,22 @@ from challenges.sorts.merge_sort_array import merge_sort
 
 
 def find_duplicate(nums):
-    if type(nums) == list and len(nums) > 0 and type(nums[0]) == int:
+    if type(nums) == list and len(nums) > 1 and type(nums[0]) == int:
         high = len(nums)
-
-        if high == 2:
-            if nums[0] == nums[1]:
-                return nums[0]
-            else:
-                return False
-
         ordered_array = merge_sort(nums)
         low = 1
-        while low < high:
+
+        while low < high - 1:
             if ordered_array[low] == ordered_array[low - 1] or ordered_array[low] == ordered_array[low + 1]:
                 return ordered_array[low]
             low += 2
-        return False
-    return False
-    #     for i in range(len(nums)):
-    #         if ordered_array[i + count] == ordered_array[i + count - 1] or ordered_array[i + count] == ordered_array[i + count + 1]:
-    #             return ordered_array[i + count]
-    #         count += 1
+        
+        if nums[0] == nums[1]:
+            return nums[0]
+        else:
+            return False
 
-    #     return False
-    # else:
-    #     return False
+    return False
 
 
 nums = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
