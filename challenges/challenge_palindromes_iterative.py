20a9378e47b38f4ca4563394ee0b8a0b6dbd2570
nato-re <alvares.renato@hotmail.com>
2021-01-08 18:26:31 -0300

    remove rpint

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,12 +3,8 @@ def is_palindrome_iterative(word):
         return False
     for i in range(len(word)):
         left, right = word[i], word[len(word) - i - 1]
-        print(left, right)
         mid = len(word) // 2
         if i >= mid:
             return True
         if left != right:
             return False
-
-
-is_palindrome_iterative("ABBA")
