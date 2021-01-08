cd46b0722f2a41c0ed9c77cd17c702809bd292f7
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-08 15:57:47 -0300

    anagram passaing

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,9 @@
-from challenges.sorts.merge_sort_string import merge_sort
+from challenges.sorts.merge_sort_array import merge_sort
 
 
 def is_anagram(first_string, second_string):
-    string_one_sorted = merge_sort(first_string)
-    string_two_sorted = merge_sort(second_string)
+    string_one_sorted = ''.join(merge_sort(list(first_string)))
+    string_two_sorted = ''.join(merge_sort(list(second_string)))
 
     if string_one_sorted == string_two_sorted:
         return True
