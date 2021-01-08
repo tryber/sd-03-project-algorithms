835219cf2ed95a92c59340535de119b536025565
luizfernando <luizfernandotic@gmail.com>
2021-01-08 16:15:20 -0300

    correção de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,13 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
-  if len(word) == 0:
+    if len(word) == 0:
         return False
     if low == high:
         return True
     if word[low] != word[high]:
         return False
     return is_palindrome_recursive(word, low + 1, high - 1)
+
 
 """ # para testes locais
 word = "socos"
