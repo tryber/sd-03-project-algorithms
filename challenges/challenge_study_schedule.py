c06837f7f19069b0a8fc4584551beda5365ae2a3
Gustavo <ggteodor07@gmai.com>
2021-01-09 06:02:11 -0300

    correcting First problem

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
+
+    if len(start_time) < 1 or len(target_time) < 1:
+        return 0
+
     count = 0
     for index, hour in start_time:
         if hour <= start_time and start_time <= end_time[index]:
