0fd2c30d3a871df891508fd9b87b7e1d1a9f15be
Gustavo <ggteodor07@gmai.com>
2021-01-09 08:50:07 -0300

    4th problem

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    cover = high - low
+    index = 0
+
+    while index < (cover // 2):
+        if word[(low + index)] != word[(high - index)]:
+            return False
+
+        index += 1
+
+    return True
