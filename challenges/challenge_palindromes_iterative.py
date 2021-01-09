1037b84cca5f6e17fe984236970bcbc14b5cc799
Gustavo <ggteodor07@gmai.com>
2021-01-09 09:16:59 -0300

    fixing problems 2.1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,14 +1,26 @@
 def is_palindrome_iterative(word):
     if word == '':
         return False
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+
+    return is_palindrome_iterative(word[1:-1])
+
+
+"""
+def is_palindrome_iterative(word):
+    if word == '':
+        return False
 
     cover = len(word) - 1
     index = 0
 
-    while index < (cover // 2):
+    while index < cover:
         if word[index] != word[(cover - index)]:
             return False
-
+        if index == (cover // 2):
+            return True
         index += 1
-
-    return True
+"""
