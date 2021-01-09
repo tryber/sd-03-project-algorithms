a1aec38be1bcc1a7eaf215365b6499f16a6dd605
Gustavo <ggteodor07@gmai.com>
2021-01-09 10:40:24 -0300

    real 4th problem

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
+from challenges.challenge_anagrams import merge_sort
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    order_nums = merge_sort(nums)
+    index = 0
+
+    for number in order_nums:
+        if number == order_nums[index + 1]:
+            return number
+        index += 1
+
+    return False
