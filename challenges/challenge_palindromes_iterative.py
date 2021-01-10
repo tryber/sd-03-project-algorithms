ef6b24408c644f1f34e2121df1fc9fb793b12b11
wscatao <wscatao@gmail.com>
2021-01-10 19:27:30 -0300

    requisito 1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    elif word == word[::-1]:
+        return True
+    else:
+        return False
