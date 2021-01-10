31c7bfb759cc3546fdfba44ba546dae2d349c7f7
potew <potew0@gmail.com>
2021-01-10 13:35:02 -0300

    Challenge no. 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,18 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    cli = 0  # Current letter index
+    lli = len(word) - 1  # Last letter index
+    palindrome = True
+    if not word:
+        return False
+    while cli < lli:
+        palindrome = palindrome and word[cli] == word[lli]
+        cli += 1
+        lli -= 1
+    return palindrome
+
+
+print(is_palindrome_iterative('AABCBAAB'))
+print(is_palindrome_iterative('ABACATE'))
+print(is_palindrome_iterative('ACAIACA'))
+print(is_palindrome_iterative('AABBAA'))
+print(is_palindrome_iterative(''))
