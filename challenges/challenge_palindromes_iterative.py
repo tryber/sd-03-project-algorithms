e886d07552c031668737b0894582f244a765786c
clayton pereira <cpereiramt@gmail.com>
2021-01-10 11:45:16 -0400

    salvando alteraçoes no requisito palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,21 @@
-def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+def is_palindrome_iterative(word, low=0, high=0):
+    if word == "":
+        return False
+    else:
+        charactere_list = list(word)
+        return organize_word(charactere_list)
+
+
+def organize_word(charactere_list):
+    inverted_word = ""
+    original_order_word = ""
+    for letter in charactere_list:
+        original_order_word += letter
+    letter_index = len(charactere_list) - 1
+    for letter in charactere_list:
+        inverted_word += charactere_list[letter_index]
+        letter_index = letter_index - 1
+    if inverted_word == original_order_word:
+        return True
+    elif not inverted_word == original_order_word:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,23 +2,6 @@ def is_palindrome_recursive(word, low, high):
     if word == "":
         return False
     else:
-        charactere_list = list(word)
-        return organize_word(charactere_list)
+        return word == word[::-1]
 
 
-def organize_word(charactere_list):
-    inverted_word = ""
-    original_order_word = ""
-    for letter in charactere_list:
-        original_order_word += letter
-    letter_index = len(charactere_list) - 1
-    for letter in charactere_list:
-        inverted_word += charactere_list[letter_index]
-        letter_index = letter_index - 1
-    if inverted_word == original_order_word:
-        return True
-    elif not inverted_word == original_order_word:
-        return False
-
-
-is_palindrome_recursive("", 0, len("") - 1)
