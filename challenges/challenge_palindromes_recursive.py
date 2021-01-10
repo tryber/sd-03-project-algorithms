75b06af537aa522e97f8bfb988fbee04b8783805
clayton pereira <cpereiramt@gmail.com>
2021-01-10 12:14:10 -0400

    salvando alteraçoes no requisito palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,6 +2,8 @@ def is_palindrome_recursive(word, low, high):
     if word == "":
         return False
     else:
-        return word == word[::-1]
-
-
+        if len(word) < 2:
+            return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome_recursive(word[1:-1], 0, len("AGUA") - 1)
