b004d4047fcbbb59792cb97ebec114bf8a81d02b
clayton pereira <cpereiramt@gmail.com>
2021-01-10 13:03:27 -0400

    salvando alteraçoes em anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,3 +2,12 @@
 def is_anagram(first_string, second_string):
     if first_string == "" or second_string == "":
         return False
+    else:
+        first_string_list = list(first_string)
+        first_string_list.sort()
+        second_string_ist = list(second_string)
+        second_string_ist.sort()
+        if first_string_list == second_string_ist:
+            return True
+        else:
+            return False
