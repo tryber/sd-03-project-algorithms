173d785fc0bf6bf406f349e4184e2b6d7f42722d
clayton pereira <cpereiramt@gmail.com>
2021-01-10 12:38:32 -0400

    salvando alteraçoes no projeto

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,21 +1,7 @@
+
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
 def is_palindrome_iterative(word, low=0, high=0):
     if word == "":
         return False
     else:
-        charactere_list = list(word)
-        return organize_word(charactere_list)
-
-
-def organize_word(charactere_list):
-    inverted_word = ""
-    original_order_word = ""
-    for letter in charactere_list:
-        original_order_word += letter
-    letter_index = len(charactere_list) - 1
-    for letter in charactere_list:
-        inverted_word += charactere_list[letter_index]
-        letter_index = letter_index - 1
-    if inverted_word == original_order_word:
-        return True
-    elif not inverted_word == original_order_word:
-        return False
+        return word == word[::-1]
