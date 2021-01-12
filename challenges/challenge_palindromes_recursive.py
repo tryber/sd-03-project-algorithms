d2a2637d776be83ffaffe50acb8e469fda71d04b
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-12 19:36:57 -0300

    Fixing recursive palindrome and passing locally

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,11 @@
 def is_palindrome_recursive(word, low, high):
-    if len(word) == 1:
+    if word == '':
+        return False
+    elif len(word) == 1:
         return True
-    elif (word[0] == word[-1]) and is_palindrome_recursive(word[1:-1]):
+    elif (
+        word[0] == word[-1] and is_palindrome_recursive(word[1:-1], low, high)
+    ):
         return True
     else:
         return False
