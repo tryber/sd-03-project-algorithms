98e935a1a2fc30731e5c02375226b4624c4edeeb
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-12 19:26:17 -0300

    Starting project

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if len(word) == 1:
+        return True
+    elif (word[0] == word[-1]) and is_palindrome_recursive(word[1:-1]):
+        return True
+    else:
+        return False
