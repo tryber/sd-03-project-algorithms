b16e04f36b25b96e064a2c0b778f191b7794f488
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-12 19:46:50 -0300

    Find the duplicate passing locally`

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    duplicate_num = 0
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if nums[index] == nums[index + 1]:
+            duplicate_num = nums[index]
+
+    if duplicate_num != 0:
+        return duplicate_num
+    
+    else:
+        return False
