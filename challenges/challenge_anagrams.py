5d294f00f2a31b24c1e8c38b5aa5a8610870cca4
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-12 20:30:40 -0300

    Passing in 4 out of 5 tests of anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
+def sorted(string):
+    array = []
+    for i in range(len(string) - 1):
+        array.append(i)
+
+    array.sort()
+
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(sorted(first_string) == sorted(second_string)):
+        return True
+    else:
+        return False
