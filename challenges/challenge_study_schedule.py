1f67f65c098167f51656ec3c5ba42b1e4537c833
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-12 20:41:42 -0300

    Passing locally in study challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    for index in range(len(start_time)):
+        if target_time >= start_time[index] and target_time <= end_time[index]:
+            counter += 1
+
+    return counter
