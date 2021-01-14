9ddb7e229f493045aefad4797aa83e0522b4fe0e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-14 20:19:51 -0300

    requisito 5: is_palindrome_iterative - melhora de tempo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,7 +2,7 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 
-    for i in range(len(word)):
+    for i in range(len(word) // 2):
         if word[i] != word[len(word) - 1 - i]:
             return False
 
