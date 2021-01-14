5aa01d2457ff698c6c813c82472778997c7958ec
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2021-01-13 23:17:54 -0300

    Palindromes

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,18 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    cur = 0
+    last = len(word) - 1
+    palindrome = True
+    if not word:
+        return False
+    while cur < last:
+        palindrome = palindrome and word[cur] == word[last]
+        cur += 1
+        last -= 1
+    return palindrome
+
+# I had a similar function in javascript about
+# palindromes so I've tried to convert it to python.
+# also, comment line too long?
+
+
+print(is_palindrome_iterative('socorrammesubinoonibusemmarrocos'))
