2739d716c288a2464dca7d948809689ff383b28a
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-14 18:44:28 -0300

    requisito 5: is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    reverse_word = ""
+
+    for i in range(len(word) - 1, -1, -1):
+        reverse_word += word[i]
+
+    return True if reverse_word == word else False
+
+
+if __name__ == "__main__":
+    print(is_palindrome_iterative(""))
+    print(is_palindrome_iterative("ANA"))
+    print(is_palindrome_iterative("SOCOS"))
+    print(is_palindrome_iterative("AGUA"))
