52eb0a3c23d5063c2dac1b159505a3f53a93bf12
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-19 18:19:39 -0300

    Req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if (not word):
+        return False
+    low = 0
+    high = len(word)
+    for i in range(len(word) // 2):
+        if (word[low] != word[high - 1]):
+            return False
+        low += 1
+        high -= 1
+    return True
