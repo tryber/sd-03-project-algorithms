bc26823f1a65e62330497052f3ca09f98d8c4351
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-19 18:03:07 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+def find_duplicate(array):
+    for i in range(len(array)):
+        current_value = array[i]
+        current_position = i
+        while (
+            current_position > 0
+            and array[current_position - 1] > current_value
+        ):
+            array[current_position] = array[current_position - 1]
+            current_position = current_position - 1
+        if (current_value == array[current_position - 1]):
+            return current_value
+        array[current_position] = current_value
+    return False
