3906a4bbd53e2d8ae54311f4841f5ca94b5f2f63
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-19 16:18:51 -0300

    Iniciando projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    cont = 0
+    if (len(end_time) != len(start_time)):
+        return 0
+    for i in range(len(start_time)):
+        if (start_time[i] <= target_time <= end_time[i]):
+            cont += 1
+    return cont
