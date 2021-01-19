b4e31ae9f7ec588ed0aa431d819c3bc78349049e
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-19 18:56:16 -0300

    palindrome refactor

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,6 +1,5 @@
 def is_palindrome_iterative(word):
-
     for i in range(len(word) // 2):
         if (word[i] != word[len(word) - i - 1]):
             return False
-    return bool(word)
+    return not(not word)
