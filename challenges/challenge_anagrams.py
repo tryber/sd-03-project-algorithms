cc037c5c9e192ccc353fdf2b3388ec67dc795c19
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-19 18:32:30 -0300

    Teste anagrama com merge sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,20 +1,53 @@
-def insertion_sort(string):
-    array = list(string)
-    for i in range(len(array)):
-        current_value = array[i]
-        current_position = i
-        while (
-            current_position > 0
-            and array[current_position - 1] > current_value
-        ):
-            array[current_position] = array[current_position - 1]
-            current_position = current_position - 1
-        array[current_position] = current_value
-    str1 = ""
-    return str1.join(array)
+def merge_sort(array):
+    # caso base: se já atingiu a menor porção (1)
+    if len(array) <= 1:
+        # retorne o array
+        return array
+    # calculo do pivot: índice que indica onde o array será particionado
+    # no caso, metade
+    mid = len(array) // 2
+    # para cada metade do array
+    # chama a função merge_sort de forma recursiva
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    # mistura as partes que foram divididas
+    return merge(left, right, array.copy())
 
+
+# função auxiliar que realiza a mistura dos dois arrays
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+
+    # enquanto nenhumas das partes é percorrida por completo
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        # compare os dois itens das partes e insira no array de mistura o menor
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    # a iteração acima irá inserir os elementos de forma ordenada
+
+    # quando uma das partes termina, devemos garantir
+    # que a outra sera totalmente inserida no array de mistura
+
+    # itera sobre os elementos restantes na partição "esquerda"
+    # inserindo-os no array de mistura
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    # itera sobre os elementos restantes na partição "direita"
+    # inserindo-os no array de mistura
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
 
 def is_anagram(first_string, second_string):
-    first_string = insertion_sort(first_string)
-    second_string = insertion_sort(second_string)
-    return first_string == second_string
+    first_string_sorted = ""
+    second_string_sorted = ""
+    first_string_sorted = first_string_sorted.join(merge_sort(list(first_string)))
+    second_string_sorted = second_string_sorted.join(merge_sort(list(second_string)))
+    return first_string_sorted == second_string_sorted
