1d766cee2eefa046d9a75b71a3e7712da3d4aba5
Rhian Castro <rhi.castro@gmail.com>
2021-01-22 17:27:15 -0300

    palíndromo + tentativa de lint. :c

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    limit = len(word) // 2
+    if not word or word[low] != word[high]:
+        return False
+    elif low > limit:
+        return True
+    else:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+
+print('Should be true:', is_palindrome_recursive('ANA', 0, len('ANA') -1 ))
+print('Should be true:', is_palindrome_recursive('SOCOS', 0, len('SOCOS') -1 ))
+print('Should be false:', is_palindrome_recursive('fawfa', 0, len('fawfa') -1 ))
