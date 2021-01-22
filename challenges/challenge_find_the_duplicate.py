5179d5a62b70582a18cd24ef2da2f150a2e3b0b9
Rhian Castro <rhi.castro@gmail.com>
2021-01-22 19:36:54 -0300

    tinkering

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if (not nums) or (len(nums) <= 1) or (type(nums[0]) == str):
+        return False
+    unique = list()
+    dup = list()
+    for i in range(len(nums)):
+        if nums[i] not in unique:
+            unique.append(nums[i])
+        else:
+            dup.append(nums[i])
+    if len(dup) == 0:
+        return False
+    return int(dup[0])
