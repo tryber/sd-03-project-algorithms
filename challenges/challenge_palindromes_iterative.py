530ffcbc8d26cd27830db405ebd04ff28dca18e9
Rhian Castro <rhi.castro@gmail.com>
2021-01-22 18:09:24 -0300

    palyndromes_iterative py + tentativa de linting

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    word_length = len(word)
+    for i in range(word_length // 2):
+        if word[i] != word[word_length -1 - i]:
+            return False
+    return True
+
+
+print(is_palindrome_iterative('ana'))
+print(is_palindrome_iterative('socos'))
+print(is_palindrome_iterative('malayam'))
