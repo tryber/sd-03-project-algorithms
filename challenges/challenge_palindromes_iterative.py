2e38f37496ff0abd33e5db167237cf96bb2a4111
wendersonbelko <contato@wendersonbelko.com>
2021-01-26 20:50:26 -0300

    vai ;-;

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,8 @@
 # referencia: https://www.w3schools.com/python/python_howto_reverse_string.asp
 def is_palindrome_iterative(word):
-    if len(word) == 0:
+    if word == "":
         return False
-    else if word == word[::-1]:
-        return True
-    else:
-        return False
+    return word == word[::-1]
+
+
+print(is_palindrome_iterative('AMA'))
