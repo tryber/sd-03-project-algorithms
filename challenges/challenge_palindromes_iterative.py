ddca2d6a0f605cd1c776b26d0ca0a877cc16c63b
wendersonbelko <contato@wendersonbelko.com>
2021-01-26 20:19:45 -0300

    removendo ultima linha e ponto e virgula

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,6 +1,6 @@
 # referencia: https://www.w3schools.com/python/python_howto_reverse_string.asp
 def is_palindrome_iterative(word):
-    word_reverse = word[::-1];
+    word_reverse = word[::-1]
     if len(word) > 0 and word == word_reverse:
         return True
     else:
