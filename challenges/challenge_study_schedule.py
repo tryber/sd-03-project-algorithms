148bdd4579ebe7e6f71bb31a1d89c8a8291c5e6b
Mateus Talles <mateustalles@gmail.com>
2021-01-27 00:42:45 -0300

    requisito 1 testando velocidade no avaliador

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
+from collections import defaultdict
+
+# estudante   1  2  3  4  5  6
+start_time = [2, 1, 2, 1, 4, 4]
+end_time   = [2, 2, 3, 5, 5, 5]
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    time_dict = defaultdict(int)
+
+
+    for index in range(0, len(start_time)):
+        for hour in range(start_time[index], end_time[index] + 1):
+            time_dict[hour] += 1
+
+    return time_dict[target_time]
+
+
+print(study_schedule(start_time, end_time, 45))
