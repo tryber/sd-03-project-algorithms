32c64c92bdc3daea19e521a5bd513c3d804ab2b0
Mateus Talles <mateustalles@gmail.com>
2021-01-27 00:59:03 -0300

    requirement 1 refactored

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,19 +1,8 @@
-from collections import defaultdict
-
-
-# estudante   1  2  3  4  5  6
-start_time = [2, 1, 2, 1, 4, 4]
-end_time = [2, 2, 3, 5, 5, 5]
-
-
 def study_schedule(start_time, end_time, target_time):
-    time_dict = defaultdict(int)
+    count = 0
 
-    for index in range(0, len(start_time)):
-        for hour in range(start_time[index], end_time[index] + 1):
-            time_dict[hour] += 1
+    for start_index, start_time in enumerate(start_time):
+        if start_time <= target_time and end_time[start_index] >= target_time:
+            count += 1
 
-    return time_dict[target_time]
-
-
-print(study_schedule(start_time, end_time, 45))
+    return count
