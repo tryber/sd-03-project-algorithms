a0e229c97a32c04fbcb645cb13bbaed2d4052f1a
wendersonbelko <contato@wendersonbelko.com>
2021-01-26 21:20:57 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,6 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    for index in range(len(nums) - 1):
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+    return False
