17a742ca05320afc422b667e3e99e9eed2f3d6c8
Mateus Talles <mateustalles@gmail.com>
2021-01-27 01:56:59 -0300

    merge sort implemented - time validation passed

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,27 +1,38 @@
-def insertion_sort(unsorted_list):
-    sorted_list = unsorted_list
-    for i in range(len(sorted_list)):
-        current_value = sorted_list[i]
-        current_position = i
-        # enquanto o valor da posição for menor que os elementos a sua esquerda
-        while (
-            current_position > 0
-            and sorted_list[current_position - 1] > current_value
-        ):
-            # move as posições para a direita
-            sorted_list[current_position] = sorted_list[current_position - 1]
-            current_position = current_position - 1
-        # colocamos o elemento em sua nova posição
-        sorted_list[current_position] = current_value
-    return sorted_list
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+        
+        for left_cursor in range(left_cursor, len(left)):
+            merged[left_cursor + right_cursor] = left[left_cursor]
+        
+        for right_cursor in range(right_cursor, len(right)):
+            merged[left_cursor + right_cursor] = right[right_cursor]
+        
+        return merged
 
 
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
 
-    sorted_first_string = insertion_sort([*first_string])
-    sorted_second_string = insertion_sort([*second_string])
+    sorted_first_string = merge_sort([*first_string])
+    sorted_second_string = merge_sort([*second_string])
 
     is_anagram = False
 
@@ -33,6 +44,3 @@ def is_anagram(first_string, second_string):
             is_anagram = True
 
     return is_anagram
-
-
-print(is_anagram('barco', 'ocrab'))
