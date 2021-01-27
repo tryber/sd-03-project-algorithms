36f883f1b8aaabaa5cf18419690e556234807000
wendersonbelko <contato@wendersonbelko.com>
2021-01-26 21:25:48 -0300

    ultimo req

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if(low >= high):
+        return True
+    if(word[low] != word[high]):
+        return False
+    return is_palindrome_recursive(word, low + 1, high - 1)
