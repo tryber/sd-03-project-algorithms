6a89b2d25dc9d08ed926d076636275feae6c0e86
gabryelryba <gabryel.ryba@hotmail.com>
2021-02-01 21:37:53 -0300

    study_schedule function passing tests locally

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    simultaneous_students = 0
+
+    if (len(start_time) == 0 or len(end_time) == 0 or target_time == 0):
+        return simultaneous_students
+
+    for i in range(len(start_time)):
+        if (end_time[i] >= target_time >= start_time[i]):
+            simultaneous_students += 1
+
+    return simultaneous_students
