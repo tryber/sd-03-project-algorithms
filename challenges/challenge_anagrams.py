0d19fbb717339373bedc09e8d921a7cfbe124523
gabryelryba <gabryel.ryba@hotmail.com>
2021-02-01 22:40:44 -0300

    anagram challenge done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string = list(first_string)
+    second_string = list(second_string)
+
+    if (first_string == [] or second_string == []):
+        return False
+
+    if (sorted(first_string) == sorted(second_string)):
+        return True
+
+    else:
+        return False
