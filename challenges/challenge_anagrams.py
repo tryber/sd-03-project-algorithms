aaf8e7e9da034ef7767357bbdd1a1ee136ee0697
DiegoRafaelAF <draf0314@gmail.com>
2021-02-06 18:18:39 -0300

    21 testes passando localmente

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui.  """
+    string_one = sorted(first_string)
+    string_two = sorted(second_string)
+    if not first_string or not second_string:
+        return False
+    if string_one == string_two:
+        return True
+    else:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    elif (low >= high):
+        return True
+    elif (word[low] == word[high]):
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    else:
+        return False
