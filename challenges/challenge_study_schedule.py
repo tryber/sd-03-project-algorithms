efef06d42b75013284a27cdab2589e54fcaf017a
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-02-10 03:41:19 -0300

    algo5

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    if not start_time or not end_time:
+    if not start_time or not end_time or not target_time:
         return 0
+    students = 0
+    for i in range(len(start_time)):
+        if(start_time[i] <= target_time <= end_time[i]):
+            students += 1
+    return students
