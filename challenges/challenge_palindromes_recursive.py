915b5e536c6a3c5fd2d40c913d53cdee34097ad9
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-02-10 03:20:04 -0300

    algo4

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,21 @@
+import math
+
+
+# Me baseei no código em https://github.com/tryber/sd-03-project-algorithms/blob/king-urek-algorithm/challenges/challenge_palindromes_recursive.py e não consegui pensar em uma forma mais elegante de apresentar
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    equal = word[low] == word[high]
+
+    if len(word) == 1:
+        return True
+
+    if low == math.ceil(len(word)/2):
+        return equal
+
+    if equal and is_palindrome_recursive(
+        word, low + 1, high - 1
+    ):
+        return True
+    else:
+        return False
